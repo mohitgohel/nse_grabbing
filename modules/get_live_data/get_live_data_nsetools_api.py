14a23f3501af12857e16515ea34af98c0b696382
Mohit Gohel <mohit@zymr.com>
2017-12-06 14:50:05 +0530

    Added multi threading in python scripts

--- modules/get_live_data/get_live_data_nsetools_api.py
+++ modules/get_live_data/get_live_data_nsetools_api.py
@@ -5,30 +5,50 @@ import pymongo
 import threading
 from datetime import datetime, time
 import itertools
+import thread
+import json
 
-def stock_data_thread(stock):
+def stock_data_thread(thread_name,stock):
     nse = Nse()
     data = nse.get_quote(stock)
-    symbol = data['symbol']    
+    symbol = data['symbol']
     client = pymongo.MongoClient('localhost',27017)
     db = client['rm_analysis']
     collection = db[symbol]
     for i in itertools.count():
         now = datetime.now()
         now_time = now.time()
-        print now_time
         if now_time >= time(9,30) and now_time <= time(15,30):
             nse = Nse()
-            data = nse.get_quote(stock)
+            try:
+                data = nse.get_quote(stock)
+            except IOError as e:
+                print "I/O error({0}): {1}".format(e.errno, e.strerror)
+                pass
             current_date_time = datetime.now()
             data['m_recorded_date_time']= current_date_time
             try:
                 collection.insert(data)
-                print "Data Entered In DB"
+                print str(symbol) + "---Data Entered In DB ---" + str(now_time)
             except:
                 print "Document already exists"
         else:
             print "Terminal Closed"
 
-
-stock_data_thread('infy')
+def multi_thread_stock_data():
+    testbed_data = json.load(open('/Users/zymr/Workspace/NSE_Grabbing/config/base_data/nse_top_100.json'))
+    print testbed_data
+    stock_list= testbed_data["nse_top_100"]
+    print stock_list
+    thread_counter= 0
+    try:
+        for each_stock in stock_list:
+            thread_counter= thread_counter+1
+            thread_name= "Thread-" + str(thread_counter)
+            thread.start_new_thread(stock_data_thread, ("thread_name",str(each_stock)))
+            print thread_name + "started"
+    except:
+        print "Unable to start thread"
+    while 1:
+        pass
+multi_thread_stock_data()
